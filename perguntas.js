0e6a7d4846fd35035413756c5b48a75bb27288e9
allwesley <wesleysilvaalmeida86@gmail.com>
2024-11-13 17:25:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Qantas Champions tem o Real Madrid?',
+    'O Real Madrid possui 15 champions'
+)
+
+criaCartao(
+    'Filmes',
+    'Quem foi o vilão de Vingadores Ultimato?',
+    'Thanos'
+)
+
+criaCartao(
+    'Musica',
+    'Quais os nomes em ordem de cada corda do violão?',
+    '1°Mi, 2°Si, 3°Sol, 4°Ré, 5°Lá, 6°Mi'
+)
+
+criaCartao(
+    'Eletronicos',
+    'Qual foi o ultimo processador lançado pela intel?',
+    'Intel Core I9'
+)
